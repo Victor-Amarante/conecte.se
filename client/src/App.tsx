b324e7bde77164c8bfc1950e88e38b8768b085f5
eduardomatos7 <eduardo.matoss@ufpe.br>
2025-12-04 20:40:53 +0055

    feat: initialize React app with Vite and Tailwind CSS

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="flex h-screen bg-black text-amber-50 justify-center items-center">
+      Começando a brincadeirinha! Hello World!
+    </div>
+  );
+}
